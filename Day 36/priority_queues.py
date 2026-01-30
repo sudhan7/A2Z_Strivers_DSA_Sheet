03432437915f839dd9324e72a31c325b39475e4c
Sudhan <rsudhan2020@gmail.com>
2026-01-30 20:27:35 +0530

    day 36 Heap

---
+++ Day 36/priority_queues.py
@@ -0,0 +1,60 @@
+class BinaryHeap:
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.arr = [0] * capacity
+    
+    def parent(self,i):
+        return i//2
+    
+    def left(self,i):
+        return 2*i+1
+    
+    def right(self,i):
+        return 2*i+2
+    
+    def insert(self,x):
+        if self.size == self.capacity:
+            print("Heap overflow")
+        
+        self.arr[self.size] = x
+        k = self.size
+        self.size+=1
+
+        while k != 0 and self.arr[self.parent(k)] > self.arr[k]:
+            self.arr[self.parent(k)], self.arr[k] = self.arr[k], self.arr[self.parent(k)]
+            k = self.parent(k)
+    
+    def heapify(self,ind):
+        li = self.left(ind)
+        ri = self.right(ind)
+
+        smallest = ind
+
+        if li < self.size and self.arr[li] < self.arr[smallest]:
+            smallest = li
+        
+        if ri < self.size and self.arr[ri] < self.arr[smallest]:
+            smallest = ri
+        
+        if smallest != ind:
+            self.arr[ind], self.arr[smallest] = self.arr[smallest], self.arr[ind]
+            self.heapify(smallest)
+
+    def print_heap(self):
+        for i in range(self.size):
+            print(self.arr[i], end=" ")
+        print()
+
+if __name__== "__main__":
+    h = BinaryHeap(20)
+    h.insert(4)
+    h.insert(1)
+    h.insert(2)
+    h.insert(6)
+    h.insert(7)
+    h.insert(3)
+    h.insert(8)
+    h.insert(5)
+
+    h.print_heap()
