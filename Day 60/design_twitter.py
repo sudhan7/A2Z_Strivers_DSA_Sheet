ab2281be515f20c0a06cdce5e93e373aab759b5a
Sudhan <rsudhan2020@gmail.com>
2026-02-25 12:28:59 +0530

    day 60 heap

---
+++ Day 60/design_twitter.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.tweetMap = defaultdict(list)
+        self.followMap = defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        self.tweetMap[userId].append([self.count, tweetId])
+        self.count -= 1
+
+    def getNewsFeed(self, userId):
+        pass
+
+    def follow(self, followerId, followeeId):
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId, followeeId):
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
+
+
+obj = Twitter()
+obj.postTweet(1,2)
+param_2 = obj.getNewsFeed(1)
+obj.follow(1,3)
+obj.unfollow(1,3)
