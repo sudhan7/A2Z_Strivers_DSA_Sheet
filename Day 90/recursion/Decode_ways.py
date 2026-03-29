4498bc67ac345c6fdf33713c4f1145431e06e2eb
Sudhan <rsudhan2020@gmail.com>
2026-03-30 00:24:30 +0530

    Day 90 recursion

--- Day 90/recursion/Decode_ways.py
+++ Day 90/recursion/Decode_ways.py
@@ -1,5 +1,15 @@
 def decode_ways(s):
-    pass
-
+    def ways(i):
+        if i == len(s):
+            return 1
+        if s[i] == 0:
+            return 0
+        
+        result = ways(i+1)
+        if i+1 < len(s) and int(s[i:i+2]) <= 26:
+            result += ways(i+2)
+        return result
+    return ways(0)
+ 
 s = "226"
 print(decode_ways(s))
