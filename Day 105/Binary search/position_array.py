c053aec987a31c22e7d2617fc9de0376859731de
Sudhan <rsudhan2020@gmail.com>
2026-04-22 12:28:27 +0530

    Day 105 binary search

---
+++ Day 105/Binary search/position_array.py
@@ -0,0 +1,26 @@
+def search_range(nums,target):
+    left = binarySearch(nums,target,True)
+    right = binarySearch(nums,target,False)
+    return [left, right]
+
+def binarySearch(nums, target, leftBias):
+    l,r = 0, len(nums)-1
+    i = -1
+    while l<=r:
+        m = (l+r)//2
+        if target > nums[m]:
+            l = m+1
+        elif target < nums[m]:
+            r = m-1
+        else:
+            i = m
+            if leftBias:
+                r = m-1
+            else:
+                l = m+1
+    return i
+
+
+nums = [5,7,7,8,8,10]
+target = 8
+print(search_range(nums,target))
