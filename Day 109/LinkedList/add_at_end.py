8772e5df6f327a514a53d2b5b4cb4f6e5652a61c
Sudhan <rsudhan2020@gmail.com>
2026-04-26 23:52:36 +0530

    Day 109 linkedlist

---
+++ Day 109/LinkedList/add_at_end.py
@@ -0,0 +1,32 @@
+   
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def insertAtEnd(head, x):
+    new_node = Node(x)
+    
+    if head is None:
+        return new_node
+    
+    temp = head
+    while temp.next != None:
+        temp = temp.next
+    temp.next = new_node
+    return head
+    
+ll = Node(1)          
+ll.next = Node(2)     
+
+
+insertAtEnd(ll, 3)  
+
+temp = ll
+while temp:
+    print(temp.data, end=" -> ")
+    temp = temp.next
+print("None")
+# Output: 1 -> 2 -> 3 -> None
+        
