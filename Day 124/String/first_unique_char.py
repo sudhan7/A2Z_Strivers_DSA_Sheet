a83921aeb8d52d0085e5bf771cf2adbced03ffe9
Sudhan <rsudhan2020@gmail.com>
2026-05-22 11:04:50 +0530

    day 124 string

---
+++ Day 124/String/first_unique_char.py
@@ -0,0 +1,20 @@
+def first_unique(s):
+    unique = {}
+
+    for i, char in enumerate(s):
+        if char not in unique:
+            unique[char] = [1,i]
+        else:
+            unique[char][0] += 1
+    
+    for key,val in unique.items():
+        if val[0] == 1:
+            return val[1]
+    return -1
+
+s = "leetcode"
+print(first_unique(s))
+s = "loveleetcode"
+print(first_unique(s))
+s = "aabb"
+print(first_unique(s))
