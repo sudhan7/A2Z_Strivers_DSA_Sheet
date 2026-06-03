430a79229eef1b5d99cf66a39a00473f63079ea1
Sudhan <rsudhan2020@gmail.com>
2026-06-03 11:54:37 +0530

    day 129 heap

---
+++ Day 129/Heap/kth_largest.py
@@ -0,0 +1,18 @@
+import heapq
+def kthlargest(nums,k):
+    heap = []
+
+    for num in nums:
+        if len(heap) < k:
+            heapq.heappush(heap,num)
+        else:
+            heapq.heappushpop(heap,num)
+    return heap[0]
+
+nums = [3,2,3,1,2,4,5,5,6]
+k = 4
+print(kthlargest(nums,k))
+
+nums = [3,2,1,5,6,4]
+k = 2
+print(kthlargest(nums,k))
